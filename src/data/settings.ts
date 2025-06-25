4709eead453f70b9b29202e9a1114a88af729753
Giacomo Di Loreto (e11690) <g.diloreto@jpanik.it>
2025-06-25 12:10:44 +0200

    updated logo and texts

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
-  site: 'https://cannonball.littlesticks.dev',
-  name: 'Little Sticks',
-  title: 'Cannonball by Little Sticks | A splash page template',
-  description: 'This is a simple splash page template built with Astro by Little Sticks',
+  site: 'https://remanedilizia.it',
+  name: 'Re.Man Srl',
+  title: 'Reman Edilizia - Impresa Edile',
+  description: '',
 }
