a13acdcb37cedf8b95b283df7ecee46293103a7e
Giacomo Di Loreto (e11690) <g.diloreto@jpanik.it>
2025-06-25 15:45:07 +0200

    enabled social footer

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -2,6 +2,6 @@ export const socials = [
   {
     title: 'Instagram',
     icon: 'mdi:instagram',
-    url: 'https://www.instagram.com/jaydanurwin/',
+    url: 'https://www.instagram.com/re.man_edilizia/',
   }
 ]
